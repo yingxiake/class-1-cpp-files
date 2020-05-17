0438ebd720f72b86c87443c2401a3a28d6ee6b08
panyueyang <57025908+panyueyang@users.noreply.github.com>
2020-05-17 16:52:57 +0800

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int v,n;
+  cin >> v >> n;
+  int a[n];
+  int dp[v+1];
+  memset(dp, 0, sizeof(dp));
+/*状态转移方程为：dp[j] = max(dp[j], dp[j-a[i]]+a[i])。*/
+  for(int i=0; i<n; i++)
+  {
+    cin>>a[i];
+    for(int j=v; j>=a[i]; j--)
+    {
+      dp[j] = max(dp[j], dp[j-a[i]]+a[i]);
+    }
+    cout << v-dp[v];
+}
